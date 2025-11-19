6c6629628a9618427ce47b657b4e3b1ce75a5aa7
Pramesh Lamichhane <103353707+pramesh61@users.noreply.github.com>
2025-11-19 19:08:47 +0545

    Add files via upload

---
+++ lab_01.java
@@ -0,0 +1,12 @@
+public class lab_01 {
+    public static void main(String[] args) {
+        int number = 2;
+
+        System.out.println("Multiplication Table for " + number + ":");
+        for (int i = 1; i <=10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        System.out.println("");
+
+    }
+}
